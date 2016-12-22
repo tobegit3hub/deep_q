ff262a572d2f53d04fe3e25efac0a7aba3035d15
tobe <tobeg3oogle@gmail.com>
2016-12-22 16:51:25 +0800

    Refactor the code with reasonable variable names

--- play_game.py
+++ play_game.py
@@ -12,13 +12,12 @@ import time
 # Define parameters
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
 flags.DEFINE_integer('episode_number', 100,
                      'Number of episode to run trainer.')
-flags.DEFINE_integer('episode_step_number', 300,
+flags.DEFINE_integer('episode_step_number', 10000,
                      'Number of steps for each episode.')
-flags.DEFINE_integer("batch_size", 32,
-                     "indicates batch size in a single gpu, default is 1024")
+flags.DEFINE_integer("batch_size", 32, "The batch size for training")
 flags.DEFINE_string("checkpoint_dir", "./checkpoint/",
                     "indicates the checkpoint dirctory")
 flags.DEFINE_string("tensorboard_dir", "./tensorboard/",
@@ -27,12 +26,18 @@ flags.DEFINE_string("optimizer", "adam", "optimizer to train")
 flags.DEFINE_integer('episode_to_validate', 1,
                      'Steps to validate and print loss')
 flags.DEFINE_string("model", "dnn", "The model to train, dnn or cnn")
-flags.DEFINE_boolean("batch_normalization", False,
-                     "Use batch normalization or not")
+flags.DEFINE_boolean("enable_bn", False, "Enable batch normalization or not")
+flags.DEFINE_float("bn_epsilon", 0.001, "The epsilon of batch normalization")
 flags.DEFINE_string("mode", "train", "Opetion mode: train, inference")
 flags.DEFINE_string("gym_env", "CartPole-v0",
                     "The gym env, like 'CartPole-v0' or 'MountainCar-v0'")
+flags.DEFINE_float("discount_factor", 0.9, "Discount factor for Q-learning")
+flags.DEFINE_integer("experience_replay_size", 10000, "Relay buffer size")
+flags.DEFINE_float("exploration_exploitation_epsilon", 0.5,
+                   "The epsilon to select action")
 flags.DEFINE_boolean("render_game", True, "Render the gym in window or not")
+flags.DEFINE_float("render_sleep_time", 0.0,
+                   "Sleep time when render each frame")
 flags.DEFINE_string("model_path", "./model/", "The output path of the model")
 flags.DEFINE_integer("export_version", 1, "The version number of the model")
 
@@ -40,40 +45,33 @@ flags.DEFINE_integer("export_version", 1, "The version number of the model")
 def main():
   print("Start playing game")
 
-  GAMMA = 0.9  # discount factor for target Q
-  INITIAL_EPSILON = 0.5  # starting value of epsilon
-  FINAL_EPSILON = 0.01  # final value of epsilon
-  REPLAY_SIZE = 10000  # experience replay buffer size
-
+  # Initial Gym environement
   env = gym.make(FLAGS.gym_env)
-  replay_buffer = deque()
-  epsilon = INITIAL_EPSILON
-  state_dim = env.observation_space.shape[0]
-
-  # For CarPole, the shape is [4, 0]
-  # For Pacman, the shape is [210, 160, 3]
+  experience_replay_queue = deque()
+  action_number = env.action_space.n
+  # The shape of CarPole is [4, 0], Pacman is [210, 160, 3]
+  state_number = env.observation_space.shape[0]
   if len(env.observation_space.shape) >= 3:
-    state_dim2 = env.observation_space.shape[1]
-    state_dim3 = env.observation_space.shape[2]
+    state_number2 = env.observation_space.shape[1]
+    state_number3 = env.observation_space.shape[2]
   else:
-    state_dim2 = env.observation_space.shape[0]
-    state_dim3 = env.observation_space.shape[0]
+    state_number2 = env.observation_space.shape[0]
+    state_number3 = env.observation_space.shape[0]
 
-  action_dim = env.action_space.n
-
-  # Define the model
-  def dnn_inference(inputs):
-    # The inputs is [BATCH_SIZE, state_dim], outputs is [BATCH_SIZE, action_dim]
+  # Define dnn model
+  def dnn_inference(inputs, is_train=True):
+    # The inputs is [BATCH_SIZE, state_number], outputs is [BATCH_SIZE, action_number]
     hidden1_unit_number = 20
     with tf.variable_scope("fc1"):
-      weights = tf.get_variable("weight", [state_dim, hidden1_unit_number],
+      weights = tf.get_variable("weight",
+                                [state_number, hidden1_unit_number],
                                 initializer=tf.random_normal_initializer())
-      bias = tf.get_variable("bias", [hidden1_unit_number],
+      bias = tf.get_variable("bias",
+                             [hidden1_unit_number],
                              initializer=tf.random_normal_initializer())
       layer = tf.add(tf.matmul(inputs, weights), bias)
 
-    # Batch normalization
-    if FLAGS.batch_normalization:
+    if FLAGS.enable_bn and is_train:
       mean, var = tf.nn.moments(layer, axes=[0])
       scale = tf.get_variable("scale",
                               hidden1_unit_number,
@@ -81,30 +79,33 @@ def main():
       shift = tf.get_variable("shift",
                               hidden1_unit_number,
                               initializer=tf.random_normal_initializer())
-      epsilon = 0.001
       layer = tf.nn.batch_normalization(layer, mean, var, shift, scale,
-                                        epsilon)
+                                        FLAGS.bn_epsilon)
 
     layer = tf.nn.relu(layer)
 
     with tf.variable_scope("fc2"):
-      weights = tf.get_variable("weight", [hidden1_unit_number, action_dim],
+      weights = tf.get_variable("weight",
+                                [hidden1_unit_number, action_number],
                                 initializer=tf.random_normal_initializer())
-      bias = tf.get_variable("bias", [action_dim],
+      bias = tf.get_variable("bias",
+                             [action_number],
                              initializer=tf.random_normal_initializer())
     layer = tf.add(tf.matmul(layer, weights), bias)
 
     return layer
 
-  def cnn_inference(inputs):
-    BATCH_SIZE = FLAGS.batch_size
-    LABEL_SIZE = action_dim
+  # Define cnn model
+  def cnn_inference(inputs, is_train=True):
+    LABEL_SIZE = action_number
 
-    # The inputs is [BATCH_SIZE, 210, 160, 3], outputs is [BATCH_SIZE, action_dim]
+    # The inputs is [BATCH_SIZE, 210, 160, 3], outputs is [BATCH_SIZE, action_number]
     with tf.variable_scope("conv1"):
-      weights = tf.get_variable("weights", [3, 3, 3, 32],
+      weights = tf.get_variable("weights",
+                                [3, 3, 3, 32],
                                 initializer=tf.random_normal_initializer())
-      bias = tf.get_variable("bias", [32],
+      bias = tf.get_variable("bias",
+                             [32],
                              initializer=tf.random_normal_initializer())
 
       # Should not use polling
@@ -115,12 +116,13 @@ def main():
       layer = tf.nn.bias_add(layer, bias)
       layer = tf.nn.relu(layer)
 
-
     # The inputs is [BATCH_SIZE, 210, 160, 32], outputs is [BATCH_SIZE, 210, 160, 64]
     with tf.variable_scope("conv2"):
-      weights = tf.get_variable("weights", [3, 3, 32, 64],
+      weights = tf.get_variable("weights",
+                                [3, 3, 32, 64],
                                 initializer=tf.random_normal_initializer())
-      bias = tf.get_variable("bias", [64],
+      bias = tf.get_variable("bias",
+                             [64],
                              initializer=tf.random_normal_initializer())
 
       layer = tf.nn.conv2d(layer,
@@ -135,34 +137,38 @@ def main():
 
     # Full connected layer result: [BATCH_SIZE, LABEL_SIZE]
     with tf.variable_scope("fc1"):
-      weights = tf.get_variable("weights", [210 * 160 * 64, LABEL_SIZE],
+      weights = tf.get_variable("weights",
+                                [210 * 160 * 64, LABEL_SIZE],
                                 initializer=tf.random_normal_initializer())
-      bias = tf.get_variable("bias", [LABEL_SIZE],
+      bias = tf.get_variable("bias",
+                             [LABEL_SIZE],
                              initializer=tf.random_normal_initializer())
       layer = tf.add(tf.matmul(layer, weights), bias)
 
     return layer
 
+  # Define train op
   model = FLAGS.model
   print("Use the model: {}".format(model))
   if model == "dnn":
-    state_input = tf.placeholder("float", [None, state_dim])
+    states_placeholder = tf.placeholder(tf.float32, [None, state_number])
     inference = dnn_inference
   elif model == "cnn":
-    state_input = tf.placeholder("float", [None, state_dim, state_dim2,
-                                           state_dim3])
+    states_placeholder = tf.placeholder(tf.float32,
+                                        [None, state_number, state_number2,
+                                         state_number3])
     inference = cnn_inference
   else:
     print("Unknow model, exit now")
     exit(1)
 
-  # Train op
-  Q_value = inference(state_input)
-
-  action_input = tf.placeholder("float", [None, action_dim])
-  y_input = tf.placeholder("float", [None])
-  Q_action = tf.reduce_sum(tf.mul(Q_value, action_input), reduction_indices=1)
-  loss = tf.reduce_mean(tf.square(y_input - Q_action))
+  logit = inference(states_placeholder, True)
+  actions_placeholder = tf.placeholder(tf.float32, [None, action_number])
+  predict_rewords = tf.reduce_sum(
+      tf.mul(logit, actions_placeholder),
+      reduction_indices=1)
+  rewards_placeholder = tf.placeholder(tf.float32, [None])
+  loss = tf.reduce_mean(tf.square(rewards_placeholder - predict_rewords))
 
   learning_rate = FLAGS.learning_rate
   print("Use the optimizer: {}".format(FLAGS.optimizer))
@@ -182,19 +188,22 @@ def main():
     print("Unknow optimizer: {}, exit now".format(FLAGS.optimizer))
     exit(1)
 
-  global_step = tf.Variable(0, name='global_step', trainable=False)
+  global_step = tf.Variable(0, name="global_step", trainable=False)
   train_op = optimizer.minimize(loss, global_step=global_step)
-
-  batch_actions = tf.argmax(Q_value, 1)
-  choose_action = batch_actions[0]
+  # Get the action with most rewoard when giving the state
+  batch_best_actions = tf.argmax(logit, 1)
+  best_action = batch_best_actions[0]
+  batch_best_q = tf.reduce_max(logit, 1)
+  best_q = batch_best_q[0]
 
   if not os.path.exists(FLAGS.checkpoint_dir):
     os.makedirs(FLAGS.checkpoint_dir)
   checkpoint_file = FLAGS.checkpoint_dir + "/checkpoint.ckpt"
   init_op = tf.initialize_all_variables()
   saver = tf.train.Saver()
-  tf.scalar_summary('loss', loss)
+  tf.scalar_summary("loss", loss)
 
+  # Create session
   with tf.Session() as sess:
     summary_op = tf.merge_all_summaries()
     writer = tf.train.SummaryWriter(FLAGS.tensorboard_dir, sess.graph)
@@ -204,86 +213,93 @@ def main():
       # Restore from checkpoint if it exists
       ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
       if ckpt and ckpt.model_checkpoint_path:
-        print(
-            "Restore model from the file {}".format(ckpt.model_checkpoint_path)
-        )
+        print("Restore model from the file {}".format(
+            ckpt.model_checkpoint_path))
         saver.restore(sess, ckpt.model_checkpoint_path)
 
       for episode in range(FLAGS.episode_number):
         # Start new epoisode to train
-        print("Start to train with episode: {}".format(episode))
         state = env.reset()
-        loss_value = -999
+        loss_value = -1
 
         for step in xrange(FLAGS.episode_step_number):
-
           # Get action from exploration and exploitation
-          if random.random() <= epsilon:
-            action = random.randint(0, action_dim - 1)
+          if random.random() <= FLAGS.exploration_exploitation_epsilon:
+            action = random.randint(0, action_number - 1)
           else:
-            action = sess.run(choose_action, feed_dict={state_input: [state]})
+            action = sess.run(best_action,
+                              feed_dict={states_placeholder: [state]})
 
+          # Run this action on this state
           next_state, reward, done, _ = env.step(action)
 
           # Get new state add to replay experience queue
-          one_hot_action = np.zeros(action_dim)
+          one_hot_action = np.zeros(action_number)
           one_hot_action[action] = 1
-          replay_buffer.append((state, one_hot_action, reward, next_state,
-                                done))
-          if len(replay_buffer) > REPLAY_SIZE:
-            replay_buffer.popleft()
+          experience_replay_queue.append((state, one_hot_action, reward,
+                                          next_state, done))
+          if len(experience_replay_queue) > FLAGS.experience_replay_size:
+            experience_replay_queue.popleft()
 
-          # Get batch replay experience to train
-          if len(replay_buffer) > FLAGS.batch_size:
+          # Get enough data to train with batch
+          if len(experience_replay_queue) > FLAGS.batch_size:
 
-            minibatch = random.sample(replay_buffer, FLAGS.batch_size)
-            state_batch = [data[0] for data in minibatch]
-            action_batch = [data[1] for data in minibatch]
-            reward_batch = [data[2] for data in minibatch]
-            next_state_batch = [data[3] for data in minibatch]
+            # Get batch experience replay to train
+            batch_data = random.sample(experience_replay_queue,
+                                       FLAGS.batch_size)
+            batch_states = []
+            batch_actions = []
+            batch_rewards = []
+            batch_next_states = []
+            expected_rewards = []
+            for experience_replay in batch_data:
+              batch_states.append(experience_replay[0])
+              batch_actions.append(experience_replay[1])
+              batch_rewards.append(experience_replay[2])
+              batch_next_states.append(experience_replay[3])
 
-            y_batch = []
-
-            Q_value_batch = sess.run(Q_value,
-                                     feed_dict={state_input: next_state_batch})
-
-            for i in range(0, FLAGS.batch_size):
-              done = minibatch[i][4]
+              # Get expected reword
+              done = experience_replay[4]
               if done:
-                y_batch.append(reward_batch[i])
+                expected_rewards.append(experience_replay[2])
               else:
-                y_batch.append(reward_batch[i] + GAMMA * np.max(Q_value_batch[
-                    i]))
+                # TODO: need to optimizer and compute within TensorFlow
+                next_best_q = sess.run(
+                    best_q,
+                    feed_dict={states_placeholder: [experience_replay[3]]})
+                expected_rewards.append(experience_replay[2] +
+                                        FLAGS.discount_factor * next_best_q)
 
-            _, loss_value, step = sess.run([train_op, loss, global_step],
-                                           feed_dict={
-                                               y_input: y_batch,
-                                               action_input: action_batch,
-                                               state_input: state_batch
-                                           })
+            _, loss_value, step = sess.run(
+                [train_op, loss, global_step],
+                feed_dict={
+                    rewards_placeholder: expected_rewards,
+                    actions_placeholder: batch_actions,
+                    states_placeholder: batch_states
+                })
 
           else:
             print("Add more data to train with batch")
 
           state = next_state
           if done:
-            print("Done for this episode with these steps to train")
             break
 
+        # Validate for some episode
         if episode % FLAGS.episode_to_validate == 0:
-          # Validate for some episode
-          print("Start to validate for episode: {}".format(episode))
-          print("Global step: {}, the loss: {}".format(step, loss_value))
+          print("Episode: {}, global step: {}, the loss: {}".format(
+              episode, step, loss_value))
 
           state = env.reset()
           total_reward = 0
 
-          for j in xrange(FLAGS.episode_step_number):
+          for i in xrange(FLAGS.episode_step_number):
             if FLAGS.render_game:
-              # time.sleep(0.1)
+              time.sleep(FLAGS.render_sleep_time)
               env.render()
 
-            action = sess.run(choose_action, feed_dict={state_input: [state]})
+            action = sess.run(best_action,
+                              feed_dict={states_placeholder: [state]})
             state, reward, done, _ = env.step(action)
             total_reward += reward
             if done:
@@ -297,15 +313,15 @@ def main():
       model_exporter.init(sess.graph.as_graph_def(),
                           named_graph_signatures={
                               'inputs': exporter.generic_signature({
-                                  "states": state_input
+                                  "states": states_placeholder
                               }),
                               'outputs': exporter.generic_signature({
-                                  "actions": batch_actions
+                                  "actions": batch_best_actions
                               })
                           })
       model_exporter.export(FLAGS.model_path,
                             tf.constant(FLAGS.export_version), sess)
-      print 'Done exporting!'
+      print "Done exporting!"
 
     elif FLAGS.mode == "untrained":
       total_reward = 0
@@ -313,7 +329,7 @@ def main():
 
       for i in xrange(FLAGS.episode_step_number):
         if FLAGS.render_game:
-          time.sleep(0.1)
+          time.sleep(FLAGS.render_sleep_time)
           env.render()
         action = env.action_space.sample()
         next_state, reward, done, _ = env.step(action)
@@ -325,14 +341,11 @@ def main():
           break
 
     elif FLAGS.mode == "inference":
-      print("Start to inference")
-
       # Restore from checkpoint if it exists
       ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
       if ckpt and ckpt.model_checkpoint_path:
-        print(
-            "Restore model from the file {}".format(ckpt.model_checkpoint_path)
-        )
+        print("Restore model from the file {}".format(
+            ckpt.model_checkpoint_path))
         saver.restore(sess, ckpt.model_checkpoint_path)
       else:
         print("Model not found, exit now")
@@ -341,12 +354,13 @@ def main():
       total_reward = 0
       state = env.reset()
 
-      for i in xrange(FLAGS.episode_step_number):
-        time.sleep(0.1)
+      index = 1
+      while True:
+        time.sleep(FLAGS.render_sleep_time)
         if FLAGS.render_game:
           env.render()
 
-        action = sess.run(choose_action, feed_dict={state_input: [state]})
+        action = sess.run(best_action, feed_dict={states_placeholder: [state]})
         next_state, reward, done, _ = env.step(action)
         state = next_state
         total_reward += reward
@@ -355,8 +369,10 @@ def main():
           print("End of inference because of done, reword: {}".format(
               total_reward))
           break
-
-      print("End of inference but not done, reword: {}".format(total_reward))
+        else:
+          if total_reward > index * 100:
+            print("Not done yet, current reword: {}".format(total_reward))
+            index += 1
 
     else:
       print("Unknown mode: {}".format(FLAGS.mode))
